fa86abf77d7214960fc8a406fa6c31f897b73234
HifzaSethi <hifzasethi@gmail.com>
2026-02-24 23:12:50 +0500

    Delete node_modules intentionally

--- TravelBookingSystem/travel-backend/middleware/admin.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (req, res, next) {
-  console.log("USER FROM TOKEN:", req.user);
-  if (req.user.role !== "admin") {
-    return res.status(403).json({ message: "Access denied. Admin only." });
-  }
-  next();
-};
